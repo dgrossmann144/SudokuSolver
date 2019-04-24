400ebd0c173bcf7ffa80601b2f76cebf98bd8009
dgrossmann144 <dgrossmann144@gmail.com>
2019-04-24 00:07:38 -0400

    Reads from input file, displays numbers, and lets you change numbers

--- src/Board.java
+++ src/Board.java
@@ -1,12 +1,50 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Point;
 
 public class Board {
     private int[][] board;
+    private boolean[][] defaultValue;
+    private Point selected = new Point(0, 0);
     
-    public Board(int[][] initValues) {
-        board = initValues;
+    public Board(int[][] initValues, boolean[][] defaultValue) {
+        this.board = initValues;
+        this.defaultValue = defaultValue;
     }
     
     public int[][] getBoard() {
         return board;
+    }
+    
+    public void drawBoard(Graphics2D g) {
+        g.setFont(new Font("Dialog", Font.PLAIN, 40));
+        for(int x = 0; x < board.length; x++) {
+            for(int y = 0; y < board[x].length; y++) {
+                if(board[y][x] != 0) {
+                    if(defaultValue[y][x]) {
+                        g.setColor(Color.BLACK);
+                    } else {
+                        g.setColor(Color.GRAY);
+                    }
+                    g.drawString(board[y][x]+"", x * SudokuSolver.WIDTH / 9 + 5, (y+1) * SudokuSolver.HEIGHT / 9 - 2);
+                }
+            }
+        }
+    }
+    
+    public void drawSelected(Graphics2D g) {
+        g.setColor(Color.YELLOW);
+        g.fillRect(selected.x * SudokuSolver.WIDTH / 9, selected.y * SudokuSolver.WIDTH / 9, SudokuSolver.WIDTH / 9, SudokuSolver.HEIGHT / 9);
+    }
+    
+    public void updateSelected(int x, int y) {
+        selected.setLocation(x / (SudokuSolver.WIDTH / 9), y / (SudokuSolver.HEIGHT / 9));
+    }
+    
+    public void typeNumber(int num) {
+        if(!defaultValue[selected.y][selected.x]) {
+            board[selected.y][selected.x] = num;
+        }
     }
 }
--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -3,15 +3,22 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class SudokuSolver extends JPanel {
     private static final long serialVersionUID = 2474932357256719501L;
-    private final int WIDTH = 300;
-    private final int HEIGHT = 300;
-    private Board board;
+    public final static int WIDTH = 300;
+    public final static int HEIGHT = 300;
+    private static Board board;
     
     public static void main(String[] args) {
         JFrame frame = new JFrame("Sudoku");
@@ -26,7 +33,86 @@ public class SudokuSolver extends JPanel {
     }
     
     public SudokuSolver() {
-        board = new Board(new int[9][9]);
+        try {
+            Scanner boardScanner = new Scanner(new File("./src/BoardInput.txt"));
+            int[][] inputBoard = new int[9][9];
+            boolean[][] defaultValue = new boolean[9][9];
+            for(int x = 0; x < 9; x++) {
+                String line = boardScanner.nextLine();
+                for(int y = 0; y < 9; y++) {
+                    inputBoard[x][y] = Integer.parseInt(line.charAt(y)+"");
+                    if(inputBoard[x][y] != 0) {
+                        defaultValue[x][y] = true;
+                    }
+                }
+            }
+            boardScanner.close();
+            board = new Board(inputBoard, defaultValue);
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+        }
+        
+        addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+                board.updateSelected(e.getX(), e.getY());
+                repaint();
+            }
+            public void mouseEntered(MouseEvent e) {
+            }
+            public void mouseExited(MouseEvent e) {
+            }
+            public void mousePressed(MouseEvent e) {
+            }
+            public void mouseReleased(MouseEvent e) {
+            }
+        });
+        
+        addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                switch(e.getKeyCode()) {
+                    case KeyEvent.VK_1:
+                        board.typeNumber(1);
+                        repaint();
+                        break;
+                    case KeyEvent.VK_2:
+                        board.typeNumber(2);
+                        repaint();
+                        break;
+                    case KeyEvent.VK_3:
+                        board.typeNumber(3);
+                        repaint();
+                        break;
+                    case KeyEvent.VK_4:
+                        board.typeNumber(4);
+                        repaint();
+                        break;
+                    case KeyEvent.VK_5:
+                        board.typeNumber(5);
+                        repaint();
+                        break;
+                    case KeyEvent.VK_6:
+                        board.typeNumber(6);
+                        repaint();
+                        break;
+                    case KeyEvent.VK_7:
+                        board.typeNumber(7);
+                        repaint();
+                        break;
+                    case KeyEvent.VK_8:
+                        board.typeNumber(8);
+                        repaint();
+                        break;
+                    case KeyEvent.VK_9:
+                        board.typeNumber(9);
+                        repaint();
+                        break;
+                    case KeyEvent.VK_0:
+                        board.typeNumber(0);
+                        repaint();
+                        break;
+                }
+            }
+        });
         
         setFocusable(true);
         requestFocus();
@@ -37,6 +123,8 @@ public class SudokuSolver extends JPanel {
         super.paintComponent(gr);
         Graphics2D g = (Graphics2D) gr;
         
+        board.drawSelected(g);
+        board.drawBoard(g);
         g.setColor(Color.BLACK);
         g.setStroke(new BasicStroke(3));
         g.drawLine(WIDTH/3, 0, WIDTH/3, HEIGHT);
